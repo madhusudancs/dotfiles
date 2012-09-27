9454f710e4b61af59186ebe8dc49ff3a20fafe50
Madhusudan.C.S <madhusudancs@gmail.com>
2012-09-27 12:45:03 -0700

    Add spyder2 config.

---
+++ spyder2/.history.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+##---(Sat Sep 22 00:55:16 2012)---
+ls
+print a
---
+++ spyder2/.temp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+/home/madhu/.spyder2/.temp.py
+"""
+
