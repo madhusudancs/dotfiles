76bbf9c6c3cf4634e71bbc872226a0b35d1970b1
Madhusudan.C.S <madhusudancs@gmail.com>
2015-12-12 23:59:51 -0800

    Add fish initialization settings.

--- omf/init.fish
+++ omf/init.fish
@@ -1,1 +1,10 @@
-set -U fish_user_paths $fish_user_paths ~/.local/bin
+eval (dircolors -c $HOME/.dircolors/dircolors.ansi-dark | sed 's/>&\/dev\/null$//')
+
+set -Ux EDITOR vim
+
+set -gx GOROOT $HOME/go
+set -gx GOPATH $HOME/godev
+
+set PATH $HOME/.local/bin $GOPATH/bin $GOROOT/bin $PATH
+
+. $HOME/.config/omf/virtual.fish
